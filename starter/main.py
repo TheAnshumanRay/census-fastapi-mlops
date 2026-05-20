f0e66ee35667e948ee4f9c99a65e098d0fb722ad
TheAnshumanRay <rayanshuman76@gmail.com>
2026-05-21 03:29:50 +0530

    Fixed Render import paths

--- starter/main.py
+++ starter/main.py
@@ -29,14 +29,14 @@ lb = joblib.load(
 
 
 cat_features = [
-    " workclass",
-    " education",
-    " marital-status",
-    " occupation",
-    " relationship",
-    " race",
-    " sex",
-    " native-country",
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
 ]
 
 
